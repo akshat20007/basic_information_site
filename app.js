c3640be12046d9126508c10709d3e063d585dc9e
Akshat Bahukhandi <65808820+akshat20007@users.noreply.github.com>
2022-05-03 22:51:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
